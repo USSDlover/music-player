6c888992ada43730ebfa33face21857cbe2b8e2e
Alireza Razinejad <a.razinejad@gmail.com>
2022-12-25 20:51:34 +0330

    [Feature] Update player control to be able to upload directories

--- src/components/molecules/player-controls/PlayerControls.tsx
+++ src/components/molecules/player-controls/PlayerControls.tsx
@@ -1,15 +1,22 @@
-import { FunctionComponent, useState } from 'react';
-import { Controls, BtnControl } from '../../atoms';
+import React, { FunctionComponent, useState } from 'react';
+import { Controls, BtnControl, UploadDirectory } from '../../atoms';
 
-interface PlayerControlsOptions {}
 
-export const PlayerControls: FunctionComponent<PlayerControlsOptions> = (): JSX.Element => {
+interface PlayerControlsOptions {
+    onDirectorySelect: (event: any) => void;
+}
+
+export const PlayerControls: FunctionComponent<PlayerControlsOptions> = ({ onDirectorySelect }): JSX.Element => {
     const [played, setPlayed] = useState<boolean>(false);
+    const uploaderRef = React.createRef<HTMLInputElement>();
 
     const onPlay = (): void => { setPlayed(!played) }
     const onNext = (): void => {}
     const onPrevious = (): void => {}
-    const onEject = (): void => {}
+    const onEject = (): void => {
+        if (uploaderRef.current)
+            uploaderRef.current.click();
+    }
 
     return (
         <div style={{ display: 'flex', gap: '.5rem', alignItems: 'center' }}>
@@ -17,6 +24,8 @@ export const PlayerControls: FunctionComponent<PlayerControlsOptions> = (): JSX.
             <BtnControl control={played ? Controls.pause : Controls.play} onClick={onPlay} />
             <BtnControl size='small' control={Controls.next} onClick={onNext} />
             <BtnControl size='small' control={Controls.eject} onClick={onEject} />
+            {/*@ts-ignore*/}
+            <UploadDirectory ref={uploaderRef} onDirectorySelect={onDirectorySelect} />
         </div>
     );
 }
